94c66feeff1a8b156ca8f64f5ec2807c3689968a
Benjamin Hawkyard <ben@hassle.com>
2015-01-31 14:30:11 +0000

    Added ember-data tried to follow tutorial

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,44 @@
-App = Ember.Application.create();
+window.Todos = Ember.Application.create();
 
-App.Router.map(function() {
+Todos.Router.map(function() {
   // put your routes here
 });
 
-App.IndexRoute = Ember.Route.extend({
+// Todos.ApplicationAdapter = DS.FirebaseAdapter.extend({
+//   firebase: new Firebase('https://glaring-torch-5894.firebaseio.com/')
+// });
+
+Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
+
+Todos.TodosRoute = Ember.Route.extend({
   model: function() {
-    return ['red', 'yellow', 'blue'];
+    return this.store.find('todo');
   }
 });
+
+Todos.Todo = DS.Model.extend({
+  title: DS.attr('string'),
+  isCompleted: DS.attr('boolean')
+});
+
+Todos.Router.map(function() {
+  this.resource('todos', { path: '/' });
+});
+
+Todos.Todo.FIXTURES = [
+ {
+   id: 1,
+   title: 'Learn Ember.js',
+   isCompleted: true
+ },
+ {
+   id: 2,
+   title: '...',
+   isCompleted: false
+ },
+ {
+   id: 3,
+   title: 'Profit!',
+   isCompleted: false
+ }
+];
